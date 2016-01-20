ea5692ada7cee6aecf92684f61f1645ba8024d9b
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-01-21 07:56:20 +0900

    Write in the current codepage if stdout is not a tty (Fix #1)

--- lstclone.go
+++ lstclone.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+
+	"github.com/mattn/go-isatty"
+	"github.com/zetamatta/nyagos/dos"
 )
 
 var hash = map[string]map[int64][]string{}
@@ -32,17 +35,43 @@ func main() {
 	for _, arg1 := range os.Args[1:] {
 		filepath.Walk(arg1, eachfile)
 	}
+
+	isatty1 := isatty.IsTerminal(os.Stdout.Fd())
+	var newline []byte
+	if isatty1 {
+		newline = []byte{'\n'}
+	} else {
+		newline = []byte{'\r', '\n'}
+	}
+
 	for _, entry1 := range hash {
 		for size, list1 := range entry1 {
 			if len(list1) <= 1 || size <= 0 {
 				continue
 			}
 			for i, path := range list1 {
+				var s string
 				if i == 0 {
-					fmt.Printf("\nrem \"%s\"\n", path)
+					os.Stdout.Write(newline)
+					s = fmt.Sprintf("rem \"%s\"", path)
 				} else {
-					fmt.Printf("del \"%s\"\n", path)
+					s = fmt.Sprintf("del \"%s\"", path)
 				}
+				if isatty1 {
+					os.Stdout.WriteString(s)
+				} else {
+					if data, err := dos.UtoA(s); err != nil {
+						fmt.Fprintf(os.Stderr, "%s: %s\n", path, err)
+						break
+					} else {
+						// trim '\0'
+						length := len(data)
+						if length >= 2 {
+							os.Stdout.Write(data[:length-1])
+						}
+					}
+				}
+				os.Stdout.Write(newline)
 			}
 		}
 	}
